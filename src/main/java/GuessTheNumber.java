a919c6afba11cfe7423a0d5534d98be1c649a68e
Daniel Commins <danel.commins@techelevator.com>
2023-03-03 13:50:50 -0800

    initial draft

---
+++ src/main/java/GuessTheNumber.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+
+public class GuessTheNumber {
+
+    // Create a main() method
+
+        // Use the following code to create a random number from 1 to 100:
+        // Note: you may have to import the Random class
+        // int randomNumber = new Random().nextInt(100) + 1;
+
+        // Create a Scanner object
+
+
+        // Create a variable to hold the number of guess attempts, example
+        // int guessAttempts = 6;
+
+
+        // While the user has more than 0 guess attempts...
+
+            // Ask the user for a number between 1 and 100
+
+
+            // If the user's number matches the random number tell the user they guessed correctly!
+            // Stop the program using:
+            // System.exit(0);
+
+
+            // Otherwise, if the user's number is lower than the random number,
+            // tell them they guessed too low
+
+                // Decrease the number of attempts by 1
+
+
+            // Otherwise, tell the user they guessed too high
+
+                // Decrease the number of attempts by 1
+
+
+        // Outside the loop, tell the user they have lost and reveal the random number!
+
+
+
+        /*
+         * Feel free to add any additional rules :)
+         */
+}
